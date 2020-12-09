22afe46b2073abd160e588f3f83bc5b94af7bab5
jeondonghwan <dh6939@naver.com>
2020-12-09 16:32:27 +0900

    wer

---
+++ TestTravis/src/TravisTest/TravisTest.java
@@ -0,0 +1,15 @@
+package TravisTest;
+
+public class TravisTest {
+	  private int num;
+	  private int val;
+	  public TravisTest(){
+	  }
+	  public TravisTest(int num, int val){
+	    this.num=num;
+	    this.val=val;
+	  }
+	  public int getNum(){
+	    return num;
+	  }
+	}
