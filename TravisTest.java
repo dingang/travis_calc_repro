5a080015c6743f4e24cd4cf4422793f7db186489
jeondonghwan <dh6939@naver.com>
2020-12-09 15:55:30 +0900

    Create TravisTest.java

---
+++ TravisTest.java
@@ -0,0 +1,13 @@
+public class TravisTest {
+  private int num;
+  private int val;
+  public TravisTest(){
+  }
+  public TravisTest(int num, int val){
+    this.num=num;
+    this.val=val;
+  }
+  public int getNum(){
+    return num;
+  }
+}
